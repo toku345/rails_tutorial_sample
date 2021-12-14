48fc28099183e331fac02f58b16bd7f6fbaf878c
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2021-12-14 11:15:10 +0900

    有効なUserかどうかを確認するテストを追加した

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example User', email: 'user@example.com')
+  end
+
+  test 'shoud be valid' do
+    assert @user.valid?
+  end
 end
