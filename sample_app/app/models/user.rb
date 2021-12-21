84191874cf65abddd699f41dbb327a40289d1113
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2021-12-21 12:02:43 +0900

    User モデルで has_secure_password を使えるようにした

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
