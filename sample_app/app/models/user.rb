bf2cb27acba2c32e843fe408cb720a8c3a985d60
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2021-12-21 11:42:56 +0900

    downcase! を使って処理を書き換えた

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
 
