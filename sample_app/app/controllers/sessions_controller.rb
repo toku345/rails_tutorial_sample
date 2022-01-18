f2e9c593d4bc554d166c791678d5223300b49181
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-01-18 11:58:52 +0900

    認証処理とログイン失敗時の処理を追加した

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@
 
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # TODO: ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 end
