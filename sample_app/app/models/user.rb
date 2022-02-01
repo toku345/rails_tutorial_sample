14ac0dd81d00660b60314461708c64ddcaad584f
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-02-01 11:24:51 +0900

    ログイン状態を保持できるようにした

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -32,4 +32,8 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token)) # rubocop:disable Rails/SkipsModelValidations
   end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
