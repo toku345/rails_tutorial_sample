c3a840d2118831138d482d9752e9b7263a7f1c4f
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-02-15 11:32:26 +0900

    🔧 Rubocop offence: UsersController#logged_in_user を早期リターンを使って書き直した

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -43,9 +43,9 @@ class UsersController < ApplicationController
   end
 
   def logged_in_user
-    unless logged_in?
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
+    return if logged_in?
+
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
 end
