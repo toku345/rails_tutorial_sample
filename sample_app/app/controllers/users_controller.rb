07f8a26c29c1bcf6d1d72b5a68e0fd071a05b275
Fumitaka Tokumitsu <fumitaka.tokumitsu@classi.jp>
2022-02-15 12:27:28 +0900

    フレンドリーフォワーディングを実装した

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -46,6 +46,7 @@ class UsersController < ApplicationController
   def logged_in_user
     return if logged_in?
 
+    store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url
   end
